881ca813b18409597da51279594a53eb63eafcae
BuildTools <unconfigured@null.spigotmc.org>
2023-08-25 17:14:20 +0700

    Changed (2023/08/25)

--- src/main/java/net/codly/codlymod/block/ModBlocks.java
+++ src/main/java/net/codly/codlymod/block/ModBlocks.java
@@ -2,23 +2,33 @@ package net.codly.codlymod.block;
 
 import net.codly.codlymod.CodlyMod;
 import net.codly.codlymod.item.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.function.Supplier;
 
-public class ModBlocks {
+public class ModBlocks{
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, CodlyMod.MODID);
 
-    public static final RegistryObject<Block> LILYPAD1_BLOCK = registerBlock("lilypad1_block",() -> new Block(BlockBehaviour.Properties.copy(Blocks.LILY_PAD)));
+    public static RegistryObject<Block> LILYPAD1_BLOCK = registerBlock("lilypad1_block",() -> new WaterlilyBlock(BlockBehaviour.Properties.copy(Blocks.LILY_PAD).noCollission()));
+
 
     private static <T extends  Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
